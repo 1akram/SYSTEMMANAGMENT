d0d0afacfca5309d788fc1b077f0d26422f343f2
AKRAM <mami.akram@live.fr>
2022-02-14 19:02:37 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,46 @@
+ 
+$(document).ready( _ => {
+
+    
+    //  password input icon 
+    $('.js-eye-icon').hover(function () {
+        $(this).addClass('fa-eye color-third');
+        $( this).parent().prev().attr('type','text');
+        $(this).removeClass('fa-eye-slash'); 
+    }, function () {
+        $(this).addClass('fa-eye-slash');
+        $( this).parent().prev().attr('type','password');
+        $(this).removeClass('fa-eye color-third cursor-pointer');
+    });
+    // end  password input icon 
+    // file input 
+    $("input[type='file']").change(function() {
+        filename = this.files[0].name;
+        $('.js-files-names').text(filename);
+        console.log(filename);
+      });
+    // end file input 
+     // alert close 
+    $(".js-alert-click").click(event =>{
+        console.log($(event.target));
+        $($(event.target)).closest('div[data-alert="alert-holder"]').remove();
+    })
+    // end alert close 
+    
+   
+     
+
+    // sidbar menu active 
+    $('.js-menu-btn').click(event =>{
+        console.log('dd');
+        $('.sidebar').toggleClass('active');
+        $(event.target).toggleClass('active');
+    });
+    // end sidbar menu active 
+
+
+
+
+ 
+});
+
